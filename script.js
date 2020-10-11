fd717e53a7a27bb5eed39d7beeb9f57deb2d2f26
Sidharth <sidnagar08@gmail.com>
2020-10-11 19:32:05 +0530

    mm1_v2

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 console.log("We will rock you!");
 
-var myBtn = document.getElementById('calcBtn');
-var lamb = document.getElementById('lamb');
-var mu = document.getElementById('mu');
+var calcBtn = document.getElementById('calcBtn');
+var upBtn = document.getElementById('upBtn')
+var grpBtn = document.getElementById('grpBtn');
+var lambEl = document.getElementById('lamb');
+var muEl = document.getElementById('mu');
 
-myBtn.addEventListener('click', ()=>{
-    if(lamb.value> mu.value){
-        alert("if λ is greter than μ then queue will increase indefinitely and the system will not have a stationary distribution! ");
-    } else {
-        console.log(lamb.value, mu.value);
-        var util = lamb.value/mu.value; //utilization
-        var p0 = 1 - util; //used in other formula
-        var L = lamb.value/(mu.value-lamb.value); //expected number of jobs in the system
-        var Lq = lamb.value**2/(mu.value*(mu.value-lamb.value)); //expected number of jobs in the queue
-        var W = L/lamb.value; //average time spent in the system
-        var Wq = Lq/lamb.value; //average time spent waiting in the queue
 
-        console.log("util:", util, "L:", L, "Lq:", Lq, "W:", W, "Wq:", Wq);
+calcBtn.addEventListener('click', ()=>{
+    var lamb = parseInt(lambEl.value);
+    var mu = parseInt(muEl.value);
+    
+    if(lamb > mu){
+        alert("If λ is greter than μ then queue will increase indefinitely and the system will not have a stationary distribution!");
+        //window.location.href = "#top";
+    }else{
+        var util = (lamb/mu).toFixed(5); //utilization
+        var p0 = (1 - util).toFixed(5); //used in other formula
+        var L = (lamb/(mu-lamb)).toFixed(5); //expected number of jobs in the system
+        var Lq = (lamb**2/(mu*(mu-lamb))).toFixed(5); //expected number of jobs in the queue
+        var W = (L/lamb).toFixed(5); //average time spent in the system
+        var Wq = (Lq/lamb).toFixed(5); //average time spent waiting in the queue
+        //console.log("util:", util, "L:", L, "Lq:", Lq, "W:", W, "Wq:", Wq);
 
         document.getElementById('res1').innerText = util;
         document.getElementById('res2').innerText = L;
         document.getElementById('res3').innerText = Lq;
         document.getElementById('res4').innerText = W;
         document.getElementById('res5').innerText = Wq;
+
+        window.location.href = "#result";
     }
 });
+
+upBtn.addEventListener('click', ()=>{
+    window.location.href = "#top";
+});
+
+grpBtn.addEventListener('click', ()=>{
+    window.location.href = "#graph";
+})
